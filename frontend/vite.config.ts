8f11a4253a3ca0e8e26b15926b5a44e083b8f5d6
tempuser12347 <tempuser7788@gmail.com>
2025-09-28 21:08:34 +0900

    build dist for 0.0.3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { readFileSync } from 'fs';
-import { execSync } from 'child_process';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
 
 const getVersion = () => {
-  // return 'v0.0.2';
-  try {
-    // Get the latest git tag
-    return execSync('git describe --tags --abbrev=0').toString().trim();
-  } catch (e) {
-    console.warn('Could not get git tag, falling back to package.json version.');
-    // Fallback to package.json version
-    return JSON.parse(readFileSync('./package.json', 'utf-8')).version;
-  }
+  return 'v' + JSON.parse(readFileSync("./package.json", "utf-8")).version;
 };
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_APP_VERSION': JSON.stringify(getVersion()),
+    "import.meta.env.VITE_APP_VERSION": JSON.stringify(getVersion()),
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
-    }
-  }
+      "/api": "http://localhost:8000",
+    },
+  },
 });
