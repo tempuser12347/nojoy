ccb6287a1fc9749569cb31d002cfe0142a70d8c4
tempuser12347 <tempuser7788@gmail.com>
2025-10-03 23:12:44 +0900

    ignore console only in production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import { readFileSync } from "fs";
 
 const getVersion = () => {
-  return 'v' + JSON.parse(readFileSync("./package.json", "utf-8")).version;
+  return "v" + JSON.parse(readFileSync("./package.json", "utf-8")).version;
 };
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   define: {
     "import.meta.env.VITE_APP_VERSION": JSON.stringify(getVersion()),
@@ -16,7 +16,9 @@ export default defineConfig({
       "/api": "http://localhost:8000",
     },
   },
-  esbuild: {
-    drop: ["console", "debugger"]
-  }
-});
+  esbuild: mode === "production"
+    ? {
+        drop: ["console"], // only drop in build
+      }
+    : {},
+}));
