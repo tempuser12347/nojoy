fb1e8cf1ad25f798b8d192a60bac0e8027cb0d8e
tempuser12347 <tempuser7788@gmail.com>
2025-10-15 10:59:01 +0900

    fix bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from './theme';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Discoveries from './pages/discovery/Discoveries';
-import Quests from './pages/quest/Quests';
-import Cities from './pages/city/Cities';
-import Recipes from './pages/recipe/Recipes';
-import Shipwrecks from './pages/shipwreck/Shipwrecks';
-import Ships from './pages/ship/Ships';
-import Consumables from './pages/consumable/Consumables';
-import TreasureMaps from './pages/treasuremap/TreasureMaps';
-import Jobs from './pages/job/Jobs';
-import Equipments from './pages/equipment/Equipments';
-import Tradegoods from './pages/tradegood/Tradegoods';
-import Certificates from './pages/certificate/Certificates';
-import Recipebooks from './pages/recipebook/RecipeBooks';
-import NotFound from './pages/NotFound';
-import ObjectDetail from './pages/object/ObjectDetail';
-import Skills from './pages/skill/Skills';
-import NpcSales from './pages/npcsale/NpcSales';
-import Regions from './pages/region/Regions';
-import TreasureBoxes from './pages/treasurebox/TreasureBoxes';
-import Fields from './pages/field/Fields';
-import Seas from './pages/sea/Seas';
-import Cultures from './pages/culture/Cultures';
-import PrivateFarms from './pages/privatefarm/PrivateFarms';
-import Nations from './pages/nation/Nations';
-import PortPermits from './pages/portpermit/PortPermits';
-import LandNpcs from './pages/landnpc/LandNpcs';
-import MarineNpcs from './pages/marinenpc/MarineNpcs';
-import Ganadors from './pages/ganador/Ganadors';
-import CityNpcs from './pages/citynpc/CityNpcs';
-import CityNpcDetail from './pages/citynpc/CityNpcDetail';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Discoveries from "./pages/discovery/Discoveries";
+import Quests from "./pages/quest/Quests";
+import Cities from "./pages/city/Cities";
+import Recipes from "./pages/recipe/Recipes";
+import Shipwrecks from "./pages/shipwreck/Shipwrecks";
+import Ships from "./pages/ship/Ships";
+import Consumables from "./pages/consumable/Consumables";
+import TreasureMaps from "./pages/treasuremap/TreasureMaps";
+import Jobs from "./pages/job/Jobs";
+import Equipments from "./pages/equipment/Equipments";
+import Tradegoods from "./pages/tradegood/Tradegoods";
+import Certificates from "./pages/certificate/Certificates";
+import Recipebooks from "./pages/recipebook/RecipeBooks";
+import NotFound from "./pages/NotFound";
+import ObjectDetail from "./pages/object/ObjectDetail";
+import Skills from "./pages/skill/Skills";
+import NpcSales from "./pages/npcsale/NpcSales";
+import Regions from "./pages/region/Regions";
+import TreasureBoxes from "./pages/treasurebox/TreasureBoxes";
+import Fields from "./pages/field/Fields";
+import Seas from "./pages/sea/Seas";
+import Cultures from "./pages/culture/Cultures";
+import PrivateFarms from "./pages/privatefarm/PrivateFarms";
+import Nations from "./pages/nation/Nations";
+import PortPermits from "./pages/portpermit/PortPermits";
+import LandNpcs from "./pages/landnpc/LandNpcs";
+import MarineNpcs from "./pages/marinenpc/MarineNpcs";
+import Ganadors from "./pages/ganador/Ganadors";
+import CityNpcs from "./pages/citynpc/CityNpcs";
+import CityNpcDetail from "./pages/citynpc/CityNpcDetail";
 
 const queryClient = new QueryClient();
 
@@ -52,7 +52,7 @@ function App() {
               <Route path="/레시피" element={<Recipes />} />
               <Route path="/침몰선" element={<Shipwrecks />} />
               <Route path="/소비품" element={<Consumables />} />
-              <Route path="/선박" element="<Ships />" />
+              <Route path="/선박" element={<Ships />} />
               <Route path="/보물지도" element={<TreasureMaps />} />
               <Route path="/직업" element={<Jobs />} />
               <Route path="/장비품" element={<Equipments />} />
